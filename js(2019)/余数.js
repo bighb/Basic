efc670dcf3c8104655876013dea56fd774fcab70
huangben <2740916018@qq.com>
2020-07-01 14:05:25 +0800

    余数

---
+++ js(2019)/余数.js
@@ -0,0 +1,10 @@
+let array=[1,2,3,4,5,6,7,8]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+     if(index % 3 ===0){
+           console.log(element);
+           
+     }
+}
+
